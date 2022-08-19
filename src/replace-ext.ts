871ec74de4b79d9041964652c4014e929b9318d8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-18 22:04:00 -0700

    test: add tests for normalizeTrim, base, and replaceExt

--- src/replace-ext.ts
+++ src/replace-ext.ts
@@ -1,1 +1,3 @@
 export { default as replaceExt } from "replace-ext"
+
+// TODO replaceExt should not change `/` to `\\` on Windows
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,5 +1,18 @@
-import * as filePaths from "../src"
+import { base, isAbsolute, join, normalizeTrim, replaceExt } from "../src"
 
 test("Node path", () => {
-  expect(filePaths.isAbsolute("/home/test1")).toBeTruthy()
+  expect(isAbsolute("/home/test1")).toBeTruthy()
+})
+
+test("normalizeTrim", () => {
+  expect(normalizeTrim("foo/bar//baz/asdf/quux/../")).toBe(join("foo", "bar", "baz", "asdf"))
+})
+
+test("base", () => {
+  expect(base("path/to/file.md")).toBe("file.md")
+  expect(base("path/to/file.md", false)).toBe("file")
+})
+
+test("replaceExt", () => {
+  expect(replaceExt("path/to/file.md", ".html")).toBe(join("./path", "to", "file.html"))
 })

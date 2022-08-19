d3a79411c024d7aad35af45b070422766bc9555a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-19 00:14:41 -0700

    chore: convert tsc's js files to mjs

---
+++ dev/tsc-mjs.mjs
@@ -0,0 +1,14 @@
+import { readdirSync } from "fs"
+import { rename } from "fs/promises"
+import { join } from "path"
+
+async function main() {
+  const dir = "./dist"
+  await Promise.all(
+    readdirSync(dir).map((file) => {
+      return rename(join(dir, file), join(dir, file.replace(".js", ".mjs")))
+    })
+  )
+}
+
+await main()

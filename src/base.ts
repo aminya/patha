af9496377ce02bf7144dcc45006594b6e03be7d5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-18 21:53:11 -0700

    fix: re-design the base API
    By default include the file extension

--- src/base.ts
+++ src/base.ts
@@ -1,21 +1,28 @@
 import { basename, extname } from "./path"
 
 /**
- * Get the base name of a path without the file extension
+ * Get the base name of a path with or without the file extension.
+ *
+ * By default the file extension is included in the base name. To remove the extension, set the second parameter to `false`.
  *
  * @param path The given file path
+ * @param includeExtension If the name should include the file extension as well
  * @returns The base name without the extension
  *
  *   Example:
  *
  *   ```js
- *   import { base, basename } from "file-paths"
+ *   import { base } from "file-paths"
  *
- *   base("path/to/file.md") // gives "file"
+ *   base("path/to/file.md") // gives "file.md"
  *
- *   basename("path/to/file.md") // gives "file.md"
+ *   base("path/to/file.md", false) // gives "file"
  *   ```
  */
-export function base(path: string) {
-  return basename(path, extname(path))
+export function base(path: string, includeExtension: boolean = true) {
+  if (includeExtension) {
+    return basename(path)
+  } else {
+    return basename(path, extname(path))
+  }
 }

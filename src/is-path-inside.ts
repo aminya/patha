dab3f8fd0f6cda4993375920ba2bb3217e7ac6cb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 16:43:54 -0700

    fix: use esmodule even for the old browser bundle

--- src/is-path-inside.ts
+++ src/is-path-inside.ts
@@ -1,1 +1,30 @@
-export { default as isPathInside } from "is-path-inside"
+import { default as isPathInsideOrig } from "is-path-inside"
+
+/**
+ * Check if a path is inside another path.
+ *
+ * Note that relative paths are resolved against `process.cwd()` to make them absolute.
+ *
+ * This function does not check if the paths exist and it only works with strings.
+ *
+ * @example
+ *
+ * ```js
+ * import { isPathInside } from "patha"
+ *
+ * isPathInside("a/b/c", "a/b")
+ * //=> true
+ *
+ * isPathInside("a/b/c", "x/y")
+ * //=> false
+ *
+ * isPathInside("a/b/c", "a/b/c")
+ * //=> false
+ *
+ * isPathInside("/Users/some/dev/aa", "/Users/some")
+ * //=> true
+ * ```
+ */
+export function isPathInside(childPath: string, parentPath: string): boolean {
+  return isPathInsideOrig(childPath, parentPath)
+}

d3cb05bd658cfc60d81134c0c5fd87615a995090
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 22:20:50 -0700

    fix: use separate entries for browser and node

---
+++ src/index-node.cjs
@@ -0,0 +1,50 @@
+import * as path from "path"
+const {
+  basename,
+  delimiter,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  normalize,
+  parse,
+  posix,
+  relative,
+  resolve,
+  sep,
+  toNamespacedPath,
+  win32,
+} = path
+
+export {
+  basename,
+  delimiter,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  normalize,
+  parse,
+  posix,
+  relative,
+  resolve,
+  sep,
+  toNamespacedPath,
+  win32,
+}
+
+export * from "./name"
+
+export * from "./normalize-trim"
+
+export * from "./add-exe-ext"
+export * from "./add-name-prefix"
+export * from "./add-name-suffix"
+export * from "./add-sh-ext"
+export * from "./add-sh-relative-prefix"
+export * from "./remove-ext"
+export * from "./replace-ext"
+
+export * from "./is-path-inside"

8fb46c029541e2d9fe648942d7fed4ef83d13433
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 22:23:03 -0700

    fix: re-export the lib function in lib.ts

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -35,16 +35,4 @@ export {
   win32,
 }
 
-export * from "./name"
-
-export * from "./normalize-trim"
-
-export * from "./add-exe-ext"
-export * from "./add-name-prefix"
-export * from "./add-name-suffix"
-export * from "./add-sh-ext"
-export * from "./add-sh-relative-prefix"
-export * from "./remove-ext"
-export * from "./replace-ext"
-
-export * from "./is-path-inside"
+export * from "./lib"

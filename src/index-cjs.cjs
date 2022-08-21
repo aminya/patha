46e8602a94c9719d87a6659e4de95f4fd9fd60b7
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 22:16:04 -0700

    fix: use import/export to re-export path in cjs

--- src/index-cjs.cjs
+++ src/index-cjs.cjs
@@ -1,4 +1,39 @@
-module.exports = require("path")
+import * as path from "path"
+const {
+  basename,
+  delimiter,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  normalize,
+  parse,
+  posix,
+  relative,
+  resolve,
+  sep,
+  toNamespacedPath,
+  win32,
+} = path
+
+export {
+  basename,
+  delimiter,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  normalize,
+  parse,
+  posix,
+  relative,
+  resolve,
+  sep,
+  toNamespacedPath,
+  win32,
+}
 
 export * from "./name"
 

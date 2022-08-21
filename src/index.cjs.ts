5211c58d8c2f7f7af0622d49234b47678ec6b02b
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 20:58:35 -0700

    fix: use separate source entries for each target

---
+++ src/index.cjs.ts
@@ -0,0 +1,15 @@
+module.exports = require("./path.js")
+
+export * from "./name"
+
+export * from "./normalize-trim"
+
+export * from "./add-exe-ext"
+export * from "./add-name-prefix"
+export * from "./add-name-suffix"
+export * from "./add-sh-ext"
+export * from "./add-sh-relative-prefix"
+export * from "./remove-ext"
+export * from "./replace-ext"
+
+export * from "./is-path-inside"

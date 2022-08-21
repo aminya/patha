66d7debb8a3eec961b2d79a02dc9e5b6efaeceee
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-08-20 22:11:04 -0700

    fix: directly re-export path from index

--- src/index-cjs.cjs
+++ src/index-cjs.cjs
@@ -1,4 +1,4 @@
-module.exports = require("./path-cjs.cjs")
+module.exports = require("path")
 
 export * from "./name"
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-export * from "./path"
+export {
+  basename,
+  delimiter,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  normalize,
+  parse,
+  posix,
+  relative,
+  resolve,
+  sep,
+  toNamespacedPath,
+  win32,
+} from "path"
 
 export * from "./name"
 
